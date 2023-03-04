bf6a063b62d071a100811e6cd3bbd8aa4efad0bf
mariolototo <mariolototo4@gmail.com>
2023-03-04 13:24:07 +0100

    feat: add buttons, form, home structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'buttons', loadChildren: () => import('./pages/buttons/buttons.module').then(m => m.ButtonsModule) },
+  { path: 'form', loadChildren: () => import('./pages/form/form.module').then(m => m.FormModule) }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
